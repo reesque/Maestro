94794665dddb6a776758c2dd1b9a59d6bab8c6d8
Reesque <akhoa.nv@gmail.com>
2025-09-18 21:09:16 -0400

    Added fullscreen mode

--- main.cpp
+++ main.cpp
@@ -7,8 +7,15 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     a.setStyle("Fusion");
     MainWindow w;
-    w.setFixedSize(800, 600);
+    //w.setFixedSize(800, 600);
     w.setWindowTitle("Maestro");
-    w.show();
+
+    QStringList args = a.arguments();
+    if (args.contains("--fullscreen")) {
+        w.showFullScreen();
+    } else {
+        w.show();
+    }
+
     return a.exec();
 }
