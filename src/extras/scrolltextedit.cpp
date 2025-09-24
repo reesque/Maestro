7c074763aaa8e1a009bda3c0ffe6256dee130b5e
Reesque <akhoa.nv@gmail.com>
2025-09-23 22:12:48 -0400

    Added auto-scroll title in now playing

---
+++ src/extras/scrolltextedit.cpp
@@ -0,0 +1,66 @@
+#include "scrolltextedit.h"
+
+#include <QScrollBar>
+
+ScrollTextEdit::ScrollTextEdit(QWidget *parent)
+    : QTextEdit(parent)
+{
+    scrollTimer = new QTimer(this);
+    startTimer = new QTimer(this);
+    endTimer = new QTimer(this);
+
+    setReadOnly(true);
+
+    // Connect all the timers
+    connect(scrollTimer, &QTimer::timeout, this, &ScrollTextEdit::scrollStep);
+    connect(startTimer, &QTimer::timeout, this, &ScrollTextEdit::waitAtTheStart);
+    connect(endTimer, &QTimer::timeout, this, &ScrollTextEdit::waitAtTheEnd);
+
+    // Start chain of timer
+    startScrolling();
+}
+
+ScrollTextEdit::~ScrollTextEdit()
+{
+    disconnect(scrollTimer, &QTimer::timeout, this, &ScrollTextEdit::scrollStep);
+    disconnect(startTimer, &QTimer::timeout, this, &ScrollTextEdit::waitAtTheStart);
+    disconnect(endTimer, &QTimer::timeout, this, &ScrollTextEdit::waitAtTheEnd);
+}
+
+void ScrollTextEdit::startScrolling()
+{
+    startTimer->start(WAIT_TIME);
+}
+
+void ScrollTextEdit::stopScrolling()
+{
+    scrollTimer->stop();
+    startTimer->stop();
+    endTimer->stop();
+    horizontalScrollBar()->setValue(0);
+}
+
+void ScrollTextEdit::waitAtTheStart()
+{
+    startTimer->stop();
+    scrollTimer->start(50);
+}
+
+void ScrollTextEdit::waitAtTheEnd()
+{
+    horizontalScrollBar()->setValue(0);
+    endTimer->stop();
+    startScrolling();
+}
+
+void ScrollTextEdit::scrollStep()
+{
+    QScrollBar* hScroll = horizontalScrollBar();
+    int newVal = hScroll->value() + 1;
+    if (newVal >= hScroll->maximum()) {
+        scrollTimer->stop();
+        endTimer->start(WAIT_TIME);
+    } else {
+        hScroll->setValue(newVal);
+    }
+}
---
+++ src/extras/scrolltextedit.h
@@ -0,0 +1,31 @@
+#ifndef SCROLLTEXTEDIT_H
+#define SCROLLTEXTEDIT_H
+
+#define WAIT_TIME 2000
+
+#include <QTextEdit>
+#include <QTimer>
+
+class ScrollTextEdit : public QTextEdit {
+    Q_OBJECT
+
+public:
+    explicit ScrollTextEdit(QWidget *parent = nullptr);
+    ~ScrollTextEdit() override;
+
+    void startScrolling();
+    void stopScrolling();
+
+private slots:
+    void scrollStep();
+    void waitAtTheStart();
+    void waitAtTheEnd();
+
+private:
+    QTimer* scrollTimer;
+    QTimer* startTimer;
+    QTimer* endTimer;
+};
+
+
+#endif // SCROLLTEXTEDIT_H
